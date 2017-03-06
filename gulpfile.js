af0e763f3b318c369c41070a0638a0d3a8d0b662
System Administrator <root@sciencesworkdeMacBook-Air.local>
2017-03-06 09:15:37 +0800

    胡同小道

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+const elixir = require('laravel-elixir');
+
+require('laravel-elixir-vue-2');
+
+/*
+ |--------------------------------------------------------------------------
+ | Elixir Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Elixir provides a clean, fluent API for defining some basic Gulp tasks
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for your application as well as publishing vendor resources.
+ |
+ */
+
+elixir((mix) => {
+    mix
+        .copy([
+            'resources/assets/lib/jquery/dist'
+        ], 'public/lib/jquery')
+        .copy([
+            'resources/assets/lib/zui/dist'
+        ], 'public/lib/jquery');
+    ;
+});
