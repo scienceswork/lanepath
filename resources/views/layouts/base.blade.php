25d3d075c3a4245d2aa755e54d09425bccc3a32b
System Administrator <root@sciencesworkdeMacBook-Air.local>
2017-03-06 12:37:44 +0800

    添加导航

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,13 +1,30 @@
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html>
+<html lang="zh-cn">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    {{-- CSRF Token--}}
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>
+        {{--网页标题--}}
+        @section('title')带你足不出户观世界@show - 胡同小道
+    </title>
+    {{--样式表，倒入基础样式表--}}
+    @section('stylesheet')
+        <link rel="stylesheet" href="{{ asset('lib/zui/css/zui.min.css') }}">
+    @show
 </head>
 <body>
-
+{{--导航--}}
+@include('layouts.partials.navbar')
+{{--内容区域--}}
+<div class="container">
+</div>
+@yield('body')
+{{--JavaScript--}}
+@section('javascript')
+    <script src="{{ asset('lib/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('lib/zui/js/zui.min.js') }}"></script>
+@show
 </body>
 </html>
---
+++ resources/views/layouts/partials/navbar.blade.php
@@ -0,0 +1,63 @@
+{{--导航--}}
+<header class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+        <!-- 导航头部 -->
+        <div class="navbar-header">
+            <!-- 移动设备上的导航切换按钮 -->
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse-lanepath">
+                <span class="sr-only">切换导航</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <!-- 品牌名 -->
+            <a href="/" class="navbar-brand">胡同小道</a>
+        </div>
+        <!-- 导航项目 -->
+        <div class="collapse navbar-collapse navbar-collapse-lanepath">
+            <!-- 一般导航项目 -->
+            <ul class="nav navbar-nav">
+                <li class="active">
+                    <a href="/">首页</a>
+                </li>
+                <li>
+                    <a href="#">作品</a>
+                </li>
+                <li>
+                    <a href="#">美食</a>
+                </li>
+                <li>
+                    <a href="#">旅游</a>
+                </li>
+                <li>
+                    <a href="#">读书</a>
+                </li>
+                <li>
+                    <a href="#">关于我们</a>
+                </li>
+            </ul>
+            <!-- 搜索 -->
+            <form action="#" class="navbar-form navbar-left" role="search">
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="搜索">
+                </div>
+                <button type="submit" class="btn btn-default">搜索</button>
+            </form>
+            <!-- 注册/登录 -->
+            <ul class="nav navbar-nav navbar-right">
+                <li>
+                    <a href="#">
+                        <i class="icon icon-user"></i>
+                        登录
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="icon icon-pencil"></i>
+                        注册
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</header>
